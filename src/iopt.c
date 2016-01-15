ef444877a6d14994069cac2a5a98dd814a54dbb1
Ryan Gonzalez <rymg19@gmail.com>
2016-01-15 10:43:12 -0600

    Optimize unused variables

--- src/iopt.c
+++ src/iopt.c
@@ -1,5 +1,18 @@
 #include "blaze.h"
 
+static void remove_unused_vars(Decl* d) {
+    int i;
+    for (i=0; i<list_len(d->vars); ++i)
+        if (d->vars[i]->uses == 0)
+            // This will cause code generators to not emit space for the variable.
+            d->vars[i]->type = NULL;
+}
+
+static void opt_decl(Decl* d) {
+    remove_unused_vars(d);
+}
+
 void iopt(Module* m) {
-    return;
+    int i;
+    for (i=0; i<list_len(m->decls); ++i) opt_decl(m->decls[i]);
 }
