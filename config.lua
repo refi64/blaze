0b0776ad9808749be820d289a3e31543c9a5557c
Ryan Gonzalez <rymg19@gmail.com>
2016-04-15 17:11:47 -0500

    Licensing

--- config.lua
+++ config.lua
@@ -1,3 +1,6 @@
+-- Any copyright is dedicated to the Public Domain.
+-- http://creativecommons.org/publicdomain/zero/1.0/
+
 config.compiler = 'clang'
 config.cflags = '-g -fcolor-diagnostics'
 config.kind = 'clang'
--- fbuildroot.py
+++ fbuildroot.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from fbuild.config.c import posix, openssl
 from fbuild.builders.pkg_config import PkgConfig
 from fbuild.builders.bison import Bison
